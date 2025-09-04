0be07d70d8cd25d70e70949e88fe6f4c359f52cb
ashwani-sinha <ashwaniraj084@gmail.com>
2025-09-04 12:07:35 +0530

    Half_product_section

--- src/landingPage/products/RightSection.js
+++ src/landingPage/products/RightSection.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
 
-function RightSection() {
-    return ( 
-        <h1>
-            RightSection
-        </h1>
-     );
+function RightSection({ productName, productDescription, learnMore, image }) {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div
+          className="col-6"
+          style={{
+            marginTop: "8%",
+            paddingRight: "15%",
+            paddingLeft: "10%",
+            lineHeight: "2",
+            wordSpacing: "1.5px",
+          }}
+        >
+          <h3>{productName}</h3>
+          <p>{productDescription}</p>
+          <a
+            href={learnMore}
+            className="fw-medium"
+            style={{ textDecoration: "none" }}
+          >
+            Learn more &rarr;
+          </a>
+        </div>
+        <div className="col-6">
+          <img src={image} alt="prodImage" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default RightSection;
